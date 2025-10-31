934eb6907b99d34c23ae2b3ac9090b5d1b61e822
Wbkim72 <wbkim@didim365.com>
2025-10-31 10:30:04 +0900

    feat: 로그인 훅 추가 및 사용자 페이지 스타일 조정 [DID-1072]
    - 로그인 API 훅(usePostLogin) 추가
    - 사용자 페이지 CardContent 패딩 조정

--- src/feature/users/components/UsersPage.tsx
+++ src/feature/users/components/UsersPage.tsx
@@ -62,7 +62,7 @@ export default function UsersPage() {
 
       {/* 검색 및 필터 */}
       <Card className="mb-6">
-        <CardContent className="pt-6">
+        <CardContent>
           <div className="flex flex-col md:flex-row gap-4">
             <div className="flex-1 relative">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-slate-400" />
