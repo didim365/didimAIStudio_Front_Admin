b1866f74e4f51cddc7dd440a9efe30e6430068f5
Wbkim72 <wbkim@didim365.com>
2025-11-03 15:25:52 +0900

    feat(users/hooks): 사용자 추가 Mutation 훅 구현 [DID-1072]

---
+++ src/feature/users/hooks/usePostUser.ts
@@ -0,0 +1,25 @@
+"use client";
+
+import { useMutation, UseMutationOptions } from "@tanstack/react-query";
+import { paths } from "@/shared/types/api/auth";
+import postUser from "../api/postUser";
+
+type PostUserRequest =
+  paths["/api/v1/users/admin/user"]["post"]["requestBody"]["content"]["application/json"];
+
+type PostUserResponse =
+  paths["/api/v1/users/admin/user"]["post"]["responses"]["201"]["content"]["application/json"];
+
+export const usePostUser = (
+  options?: Omit<
+    UseMutationOptions<PostUserResponse, Error, PostUserRequest>,
+    "mutationFn"
+  >
+) => {
+  return useMutation<PostUserResponse, Error, PostUserRequest>({
+    mutationFn: (data: PostUserRequest) => postUser(data),
+    ...options,
+  });
+};
+
+export default usePostUser;
