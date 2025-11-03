95e97defd1194861150d554b71f3df79cf633abf
Wbkim72 <wbkim@didim365.com>
2025-11-03 14:56:37 +0900

    fix: DID-1072 사이드바 메뉴가 자식 경로에서도 활성화 상태 유지되도록 수정

--- src/app/dashboard/users/[userId]/edit/page.tsx
+++ src/app/dashboard/users/[userId]/edit/page.tsx
@@ -1,17 +1,13 @@
 import { UserEditPage } from "@/feature/users/pages/UserEditPage";
 import { cookies } from "next/headers";
 import axios from "axios";
-import { redirect } from "next/navigation";
 import { BASE_URL } from "@/shared/constants";
+import { redirect } from "next/navigation";
 
 async function Page({ params }: { params: Promise<{ userId: string }> }) {
   const { userId } = await params;
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("access_token")?.value;
-
-  if (!accessToken) {
-    redirect("/");
-  }
 
   const response = await axios.get(
     `${BASE_URL}/api/v1/users/account/${userId}`,
@@ -21,6 +17,10 @@ async function Page({ params }: { params: Promise<{ userId: string }> }) {
       },
     }
   );
+
+  if (response.status === 401) {
+    redirect("/");
+  }
 
   const user = response.data;
   return <UserEditPage user={user} />;
