7bf55845f2c0327d865e60977fd4304eeb827aa9
Wbkim72 <wbkim@didim365.com>
2025-11-14 17:04:43 +0900

    feat: [DID-1109] 페르소나 데이터 조회 API 엔드포인트 수정 (/v1 제거 및 타입 정의 포맷팅 개선)

--- src/feature/personas/api/getPersonasData.ts
+++ src/feature/personas/api/getPersonasData.ts
@@ -4,12 +4,13 @@ import axiosInstance from "@/shared/utils/axiosInstance";
 type GetPersonasDataResponse =
   paths["/v1/personas/data"]["get"]["responses"]["200"]["content"]["application/json"];
 
-type GetPersonasDataParams = paths["/v1/personas/data"]["get"]["parameters"]["query"];
+type GetPersonasDataParams =
+  paths["/v1/personas/data"]["get"]["parameters"]["query"];
 
 const getPersonasData = async (params?: GetPersonasDataParams) => {
   try {
     const response = await axiosInstance.agent.get<GetPersonasDataResponse>(
-      "/v1/personas/data",
+      "/personas/data",
       { params }
     );
     return response.data;
