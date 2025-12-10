c73c7aa93efdc92d68e692f34b8dfae7df2007f3
Wbkim72 <wbkim@didim365.com>
2025-12-10 16:26:20 +0900

    DID-1198
    - PersonasPage import 경로를 templates에서 data로 변경

--- src/app/(manage)/studio/data/personas/page.tsx
+++ src/app/(manage)/studio/data/personas/page.tsx
@@ -1,4 +1,4 @@
-import PersonasPage from "@/feature/studio/templates/personas/_pages/PersonasPage";
+import PersonasPage from "@/feature/studio/data/personas/_pages/PersonasPage";
 
 function Page() {
   return <PersonasPage />;
