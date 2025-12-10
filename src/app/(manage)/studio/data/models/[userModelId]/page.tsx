6509daf2e9e5651d8b3a7cca21c73ba79b9a2a5f
Wbkim72 <wbkim@didim365.com>
2025-12-10 14:18:49 +0900

    DID-1198: SSR 환경에서 getConfig API 사용하여 데이터 패칭
    - page.tsx에서 SSR로 getConfig 호출하여 configData 가져오기
    - ModelPage props를 userModelId에서 config 데이터로 변경

--- src/app/(manage)/studio/data/models/[userModelId]/page.tsx
+++ src/app/(manage)/studio/data/models/[userModelId]/page.tsx
@@ -1,8 +1,13 @@
 import ModelPage from "@/feature/studio/data/models/[userModelId]/_pages/ModelPage";
+import getConfig from "@/feature/studio/data/models/[userModelId]/_api/getConfig";
 
 async function Page({ params }: { params: Promise<{ userModelId: string }> }) {
   const { userModelId } = await params;
-  return <ModelPage userModelId={userModelId} />;
+
+  // SSR 데이터 패칭
+  const configData = await getConfig({ user_model_id: Number(userModelId) });
+
+  return <ModelPage config={configData} />;
 }
 
 export default Page;
--- src/feature/studio/data/models/[userModelId]/_pages/ModelPage.tsx
+++ src/feature/studio/data/models/[userModelId]/_pages/ModelPage.tsx
@@ -1,8 +1,10 @@
+import { GetConfigResponse } from "../_api/getConfig";
+
 interface ModelPageProps {
-  userModelId: string;
+  config: GetConfigResponse;
 }
 
-function ModelPage({ userModelId }: ModelPageProps) {
+function ModelPage({ config }: ModelPageProps) {
   return <div>상세조회 관련 API 수정 대기 중</div>;
 }
 
