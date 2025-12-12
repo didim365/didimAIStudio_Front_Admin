82ac754f95b0a55f8a56c34ac05d276ebb372449
Wbkim72 <wbkim@didim365.com>
2025-12-12 12:44:07 +0900

    DID-1198: 모델 상세 페이지에 getCatalog API 연동
    - page.tsx에서 getCatalog API 호출하여 SSR 데이터 패칭
    - ModelPage 컴포넌트에 catalog props 타입 추가

--- src/feature/studio/templates/models/[modelId]/_pages/ModelPage.tsx
+++ src/feature/studio/templates/models/[modelId]/_pages/ModelPage.tsx
@@ -1,4 +1,10 @@
-function ModelPage() {
+import { GetCatalogResponse } from "../_api/getCatalog";
+
+interface ModelPageProps {
+  catalog: GetCatalogResponse;
+}
+
+function ModelPage({ catalog }: ModelPageProps) {
   return <div>상세조회 관련 API 수정 대기 중</div>;
 }
 
